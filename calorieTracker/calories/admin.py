6d2a11862997253479b994be5eaa7c7799a7c361
Amit Verma <amorphous4u@gmail.com>
2022-10-08 10:26:26 -0400

    Makemigrations Complete

--- calorieTracker/calories/admin.py
+++ calorieTracker/calories/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Food, Profile
 
 # Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+
+admin.site.register(Food)
+admin.site.register(Profile)
