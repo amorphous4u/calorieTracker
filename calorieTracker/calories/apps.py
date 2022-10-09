c59ccc306dc13ff004e6b1dd67704c35c48c3699
Amit Verma <amorphous4u@gmail.com>
2022-10-09 16:10:45 -0400

    Add Forms/Signals/Filters

--- calorieTracker/calories/apps.py
+++ calorieTracker/calories/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CaloriesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calories'
+
+    def ready(self):  # new
+        import calories.signals  # new
